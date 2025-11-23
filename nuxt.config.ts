5ef42e95ab338900d4ad7a4e5b51d64562b1002b
markandrevegas <markandrevega@me.com>
2025-11-23 21:23:20 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,91 @@
+import { defineNuxtConfig } from 'nuxt/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// Extend the NuxtConfig type
+declare module 'nuxt/schema' {
+  interface NuxtConfig {
+    image?: {
+      quality?: number;
+      domains?: string[];
+      providers?: {
+        [key: string]: {
+          name: string;
+          provider: string;
+          options?: {
+            baseURL?: string;
+            [key: string]: unknown;
+          };
+        };
+      };
+    };
+  }
+
+  interface NuxtOptions {
+    image?: NuxtConfig['image'];
+  }
+}
+
+export default defineNuxtConfig({
+	app: {
+		head: {
+			link: [
+				{ rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+				{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+				{ rel: "shortcut icon", href: "/favicon.ico" },
+				{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+				{ rel: "manifest", href: "/site.webmanifest" }
+			],
+			meta: [{ name: "apple-mobile-web-app-title", content: "Scratch" }]
+		}
+	},
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	css: ["@/assets/css/tailwind.css"],
+	vite: {
+		plugins: [tsconfigPaths()],
+    /* server: {
+      allowedHosts: ['scratch.local']
+    } */
+	},
+	modules: ["@nuxtjs/color-mode", "@nuxtjs/tailwindcss", "@nuxt/fonts", "@nuxt/image", "@nuxt/eslint", "@nuxt/icon"],
+	image: {
+		providers: {
+			placehold: {
+				name: "placehold",
+				provider: "ipx",
+				options: {
+					baseURL: "https://placehold.co/48x48"
+				}
+			}
+		},
+		domains: ["placehold.co"],
+		quality: 80
+	},
+	runtimeConfig: {
+		spotifyClientId: process.env.NUXT_SPOTIFY_CLIENT_ID,
+		spotifyClientSecret: process.env.NUXT_SPOTIFY_CLIENT_SECRET,
+		unsplashAccessKey: process.env.NUXT_UNSPLASH_ACCESS_KEY,
+		public: {
+			unsplashBase: "https://api.unsplash.com",
+			fonts: {
+				selfHosted: false
+			}
+		}
+	},
+	fonts: {
+		provider: "google",
+		assets: {},
+		families: [
+			{
+				name: "Lexend",
+				weights: [100, 200, 300, 400, 500, 600, 700]
+			}
+		]
+	},
+	nitro: {
+		preset: "node-server",
+		externals: {
+			inline: ["ipx", "ofetch"]
+		}
+	}
+})
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+	<ColorModeToggle />
+</template>
