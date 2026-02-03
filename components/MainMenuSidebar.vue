8437cc18ce23b5d088e769cb85048217e89789f2
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-02-03 22:24:18 +0100

    menu animation

--- components/MainMenuSidebar.vue
+++ components/MainMenuSidebar.vue
@@ -15,7 +15,7 @@
 	<div>
 		<header :class="props.isScrolled ? 'backdrop-blur-xl text-white bg-abyssal/40' : 'backdrop-blur-md text-abyssal'" class="fixed left-0 top-0 z-40 flex h-[70px] w-full items-center justify-between px-4 transition-colors duration-300">
 			<h1 class="text-5xl uppercase tracking-widest">Tailor</h1>
-			<MenuIcon @click="toggleMenu"/>
+			<MenuIcon :is-open="isOpen" @click="toggleMenu"/>
 		</header>
 		<!-- Drawer overlay -->
 		<div v-if="isOpen" class="fixed inset-0 z-40 bg-white/50" @click="toggleMenu"></div>
@@ -24,9 +24,9 @@
 				<div class="flex-1 overflow-y-scroll pb-[96px] pt-[70px]">
 					<ul class="menu px-4">
 						<li><NuxtLink to="/" @click="toggleMenu">Home</NuxtLink></li>
-						<li><NuxtLink to="/about" @click="toggleMenu">About</NuxtLink></li>
-						<li><NuxtLink to="/blog" @click="toggleMenu">Blog</NuxtLink></li>
-						<li><NuxtLink to="/contact" @click="toggleMenu">Contact</NuxtLink></li>
+						<li><NuxtLink to="/" @click="toggleMenu">About</NuxtLink></li>
+						<li><NuxtLink to="/" @click="toggleMenu">Blog</NuxtLink></li>
+						<li><NuxtLink to="/" @click="toggleMenu">Contact</NuxtLink></li>
 					</ul>
 				</div>
 				<div class="socials fixed bottom-0 left-0 right-0 z-20 flex h-24 items-center justify-start gap-4 bg-white px-4">
