9267bbd4d04e80ef4d4a43c18fba062790e8a0ef
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-02-11 14:00:30 +0100

    text and image layout in hero

--- app/components/sections/Hero.vue
+++ app/components/sections/Hero.vue
@@ -11,17 +11,17 @@ const loadingIconHeight = "2.5rem"
 </script>
 
 <template>
-	<div class="h-[calc(100vh-70px)] relative">
+	<div class="h-[calc(100vh-70px)] relative flex flex-col">
 		<div v-if="loading" class="absolute top-0 right-0 bottom-0 left-0 z-50 flex flex-col justify-center items-center bg-white">
 			<Loading :width="loadingIconWidth" :height="loadingIconHeight" />
 		</div>
-		<div class="absolute bottom-0 w-full h-3/5 z-10 overflow-hidden">
-			<NuxtImg v-if="photo" :src="photo.urls.full" :alt="photo.alt" class="object-cover" />
-			<div class="absolute inset-0 z-20 bg-black/20"></div>
-		</div>
-		<div class="h-72 text-right absolute right-0 bottom-0 left-0 z-30 p-6 text-white flex-flex-col justify-start">
+		<div class="flex-1 flex flex-col justify-end z-30 p-6">
 			<h1 class="text-3xl uppercase font-medium">{{ header }}</h1>
 			<p class="mt-2 max-w-md uppercase">{{ text }}</p>
+		</div>
+		<div class="flex-1 relative w-full z-10 overflow-hidden">
+			<NuxtImg v-if="photo" :src="photo.urls.full" :alt="photo.alt" class="object-cover w-full h-full" />
+			<div class="absolute inset-0 z-20 bg-black/20"></div>
 		</div>
 	</div>
 </template>
